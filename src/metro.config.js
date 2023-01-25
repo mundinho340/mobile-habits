1138c6ac8070091799a52024ba8f411cdf49f28d
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-25 20:53:19 -0300

    metroConfig

--- src/metro.config.js
+++ src/metro.config.js
@@ -1,22 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports =(() => {
+  const { transformer, resolver } = config;
 
-    const config= getDefaultConfig(__dirname)
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
 
-    const { tranformer, resolver } = config
-
-    config.tranformer ={
-        ...tranformer,
-        bubelTransformerPath: require.resolve("react-native-svg-transformer"),
-    }
-
-    config.resolver ={
-        ...resolver,
-        assatExts: resolver.assetExts.filter((ext)=> ext !== "svg"),
-        sourceExts: [...resolver.sourceExts, "svg"],
-    }
-
-    return config
-})()
+  return config;
+})();
