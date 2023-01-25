264843914012fd952e042fb55313522cc391a498
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-24 22:29:44 -0300

    setmobileConfig

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -8,6 +8,7 @@ import {
   Inter_800ExtraBold
 } from "@expo-google-fonts/inter"
 import { Loading } from './src/components/Loading';
+import  Home  from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded]= useFonts({
@@ -21,22 +22,11 @@ export default function App() {
     return <Loading/>;
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Raimundo </Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: '#7C3AED',
-    fontFamily:'Inter_800ExtraBold'
-  }
-});
+;
---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+import { View,Text } from "react-native"
+export default function Home(){
+    return(
+        <View>
+            {/* <Text style={styles.text}>Raimundo </Text> */}
+
+        </View>
+    )
+
+
+
+}
+
+
+
