2ac6b95b085e42c7a97d0a695d43488cc21e706a
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-25 07:47:45 -0300

    setayoutmobile

---
+++ src/@types/app.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="nativewind/types" />
+
+
+//esse ficheiro serve para fazer o react-native reconheça o className.
+//podemos chamar essas configur ações de tipagem.
+
+//npx tailwindcss init
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { View,Text } from "react-native"
 export default function Home(){
     return(
-        <View>
+        <View className="flex-1 bg-background">
             {/* <Text style={styles.text}>Raimundo </Text> */}
 
         </View>
