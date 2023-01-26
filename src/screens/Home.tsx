91a301a015602fcb77c91b65b220fa86f5dc5aa4
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-25 22:01:34 -0300

    finaly header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header"
 
 export default function Home(){
     return(
-        <View className="flex-1 bg-background">
+        <View className="flex-1 bg-background px-8 pt-16">
             <Header/>
         </View>
     )
