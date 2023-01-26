3e807a44f681bfa7010c740b75752335feaa902f
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-25 21:17:29 -0300

    j

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
- import { View } from 'react-native'
+import { View } from 'react-native'
 import Logo from "../assets/logo.svg"
- export function Header(){
+
+
+export function Header(){
     return(
         <View className='w-full flex-row items-center justify-between'>
             <Logo/>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,10 @@
 import { View,Text } from "react-native"
+import { Header } from "../components/Header"
+
 export default function Home(){
     return(
         <View className="flex-1 bg-background">
-            {/* <Text style={styles.text}>Raimundo </Text> */}
-
+            <Header/>
         </View>
     )
 
