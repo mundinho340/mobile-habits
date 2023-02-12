e2d409d58feb420ac572d96adc3af2595ca8b97a
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-12 20:37:44 -0300

    pvp

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import {
   Inter_800ExtraBold
 } from "@expo-google-fonts/inter"
 import { Loading } from './src/components/Loading';
-import  Home  from './src/screens/Home';
+import { Routes } from './src/routers';
+
 
 export default function App() {
   const [fontsLoaded]= useFonts({
@@ -23,7 +24,7 @@ export default function App() {
   }
   return (
     <>
-      <Home />
+        <Routes/>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </>
   );
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -3,3 +3,12 @@ import { NavigationContainer} from '@react-navigation/native'
 
 import { AppRoutes } from './app.routes'
 
+export function Routes(){
+    return(
+        <View className='flex-1 bg-background'>
+            <NavigationContainer>
+                <AppRoutes/>
+            </NavigationContainer>
+        </View>
+    )
+}
