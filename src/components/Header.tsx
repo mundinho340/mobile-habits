4895054f4a4d7468891a0bfdd3727a9a62c849e0
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-04 19:53:59 -0300

    routs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ import { useNavigation } from '@react-navigation/native'
 
 
 export function Header(){
+    const {navigate} = useNavigation();
     return(
         <View className='w-full flex-row items-center justify-between'>
             <Logo/>
             <TouchableOpacity
                 activeOpacity={0.7}
                 className='flex-row h-11 px-4 border border-violet-500 rounded-lg items-center'
+                onPress={()=> navigate('new')}
             >   
                 <Feather
                     name='plus'
