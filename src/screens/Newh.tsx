84c1a1ab9dda50178d4ba8a2d0240a14697fbc5b
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-10 19:11:52 -0300

    po

--- src/screens/Newh.tsx
+++ src/screens/Newh.tsx
@@ -1,16 +1,18 @@
 import { View , Text, TextInput} from "react-native";
 import {ScrollView} from 'react-native'
 import { BackButton } from "../components/BackButton";
+import { CheckBox } from "../components/CheckBox";
 
 
 export function Newh(){
+    const availableWeekDays = ["Domingo","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
     return(
           <View className="flex-1 bg-background px-8 pt-16">
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <BackButton/>
 
                     <Text className="mt-6 text-white font-extrabold text-3xl">
-                        Criar hábito
+                        Criar hábito                      
                     </Text>
 
                       <Text className="mt-6 text-white font-semibold text-base">
@@ -20,6 +22,19 @@ export function Newh(){
                     <TextInput
                         className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
                     />
+                    <Text className="font-semibold mt-4 mb-3 text-white text-base">
+                        Qual é a recorrência?
+                    </Text>
+                    {
+                        availableWeekDays.map((weekDay, index)=>(
+                        <CheckBox   
+                        checked
+                        key={weekDay}
+                        title={weekDay}
+                    />
+                        ))
+                    }
+                    
                 </ScrollView>
          </View> 
     )
