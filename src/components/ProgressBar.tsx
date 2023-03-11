a4ac0a394d4c3bc754ec79e618d2139fb4653ef3
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-11 20:36:45 -0300

    Create ProgressBar.tsx

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+interface Props{
+    progress?: number;
+}
+export function ProgressBar({progress=0}: Props){
+    <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
+        <View
+            className="w-full h-3 rounded-xl bg-zinc-700 mt-4"
+        >
+            
+        </View>
+    </View>
+}   
