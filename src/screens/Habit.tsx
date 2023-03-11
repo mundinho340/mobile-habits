4e8e3199a8f887db7aa410e4c85960f587c738dd
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-11 20:25:18 -0300

    Date

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,5 +1,8 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, View ,Text} from "react-native";
+import '../lib/dayjs'
 import { useRoute } from "@react-navigation/native";
+import { BackButton } from "../components/BackButton";
+import dayjs from "dayjs";
 interface Params {
     date: string;
 }
@@ -7,15 +10,25 @@ export function Habit(){
     const route = useRoute();
     const {date} = route.params as Params;
     console.log(date)
+    const parsedDate = dayjs(date);
+    const dayOfWeek = parsedDate.format('dddd');
+    const dayAndMonth= parsedDate.format("DD/MM");
     return(
-        <ScrollView
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{paddingBottom:100}}
-        >
-          <View className="flex-1 bg-background px-8 pt-16">
+        <View className="flex-1 bg-background px-8 pt-16">
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom:100}}
+            >
+                <BackButton/>
+                <Text className="text-zinc-400 mt-6 font-semibold text-base lowercase">
+                    {dayOfWeek}
+                </Text>
+                <Text  className="text-white font-extrabold text-3l=xl">
+                    {dayAndMonth}
+                </Text>
+            </ScrollView>
 
-         </View> 
-        </ScrollView>
+        </View> 
     )
   
 }
