92e86e287ea8f149e5b5678eba4b46410110ca4f
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-05-05 02:03:47 +0200

    Add files via upload

---
+++ MapScreen.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import MapView from "react-native-maps";
+import Map from '../components/Map';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionCard from '../components/RideOptionCard';
+
+const MapScreen = () => {
+  const Stack = createStackNavigator()
+  return (
+    <View>
+      <View style={tw`h-1/2`}>
+        <Map/>
+      </View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+            <Stack.Screen
+              name="RideOptionCard"
+              component={RideOptionCard}
+              options={{
+                headerShown: false,
+              }}
+          />
+        </Stack.Navigator>
+      </View>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
