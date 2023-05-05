31c0c44a9153c667d1153c0c19cb56bb1b2e04a1
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-05-05 02:13:49 +0200

    update map

---
+++ components/Map.js
@@ -0,0 +1,49 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MapView,{ Marker } from 'react-native-maps'
+import tw from 'tailwind-react-native-classnames'
+import { useSelector } from 'react-redux'
+import {GOOGLE_MAPS_APIKEY} from "@env"
+import MapViewDirections from "react-native-maps-directions"
+import { selectDestination, selectOrigin } from '../slices/navSlice'
+
+
+const Map = () => {
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination)
+
+  return (
+    <MapView
+    
+        style={tw`flex-1`}
+        mapType="mutedStandard"
+        initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+      {origin && destination &&(
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="green"
+        />
+      )}
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier='origin'
+        />
+      )}
+  </MapView>)
+};
+export default Map
