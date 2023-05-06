a01f414c71625f32be89c68a77ac3cd4e27af7e0
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-05-06 12:34:28 -0300

    Revert "navcard rideoptions navigateCard"
    This reverts commit b199b1c25ee81783253aacd317b3b27c1630bd18.

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,10 +2,7 @@ import {Image, TouchableOpacity, FlatList,StyleSheet, Text, View } from 'react-n
 import tw from "tailwind-react-native-classnames"
 import { Icon } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
-import { useSelector } from 'react-redux'
-import { selectOrigin } from '../slices/navSlice'
-//https://links.papareact.com/3pn
-//https://links.papareact.com/28w
+
 const data =[
     {
         id: "123",
@@ -22,7 +19,6 @@ const data =[
 ]
 
 const NavOptions = () => {
-    const origin = useSelector(selectOrigin);
     const navigation = useNavigation();
   return (
     <FlatList
@@ -32,11 +28,9 @@ const NavOptions = () => {
     renderItem={({item}) =>(
         <TouchableOpacity
             onPress={()=> navigation.navigate(item.screen)}
-            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
-            disabled={!origin}
-         >
+         style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             
-            <View style={tw`${!origin && "opacity-20"}`}>
+            <View>
                 <Image
                     style={{width: 120, height: 120, resizeMode:"contain"}}
                     source={{uri: item.image}}
